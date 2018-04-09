e64195e2b29a0655549504ee2ff387ddd2913239
HJ Park <hj.park@ims-elec.com>
2018-04-09 17:46:31 +0900

    Add files via upload

--- install_after_ubuntu_istall.sh
+++ install_after_ubuntu_istall.sh
@@ -13,7 +13,7 @@ sudo gedit /etc/samba/smb.conf
 
 sudo service smbd restart
 
-sudo apt-get install nfs-common nfs-kernel-server portmap
+sudo apt-get install -y nfs-common nfs-kernel-server portmap
 chmod 777 /home/hjpark/
 sudo gedit /etc/exports 
 #/home/hjpark 39.118.159.18 (rw,all_squash,async)
@@ -22,3 +22,12 @@ sudo gedit /etc/exports
 
 #On the client
 #mount -t nfs 211.238.111.124:/home/hjpark /mnt
+
+sudo apt install -y fslint
+sudo apt install -y filezilla
+sudo apt install -y git
+git config --global user.email "hjparkg71@gmail.com"
+git config --global user.name "hjpark"
+
+
+
